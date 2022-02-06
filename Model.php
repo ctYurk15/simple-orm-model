89dbe5812350922d419f64ccbfb4f397395d4f2c
ctYurk15 <levgenetic@gmail.com>
2022-02-06 12:24:56 +0200

    Add ORM prototype

---
+++ Model.php
@@ -0,0 +1,100 @@
+<?php
+
+class Model
+{
+ public static $table;
+ public static $index_column;
+ public static $conn;
+ public static $dbdata_path;
+
+ public $index_value;
+
+ protected $values;
+ protected $changed_values;
+
+ public static function init_conn()
+ {
+  $db_data = json_decode(file_get_contents(static::$dbdata_path), true);
+  static::$conn = new mysqli($db_data['host'], $db_data['user'], $db_data['pass'], $db_data['dbname']);
+ }
+
+ public static function all($where_clause = '')
+ {
+  $records = [];
+  $class_name = static::class;
+
+  if(static::$conn == null) static::init_conn();
+
+  $query = 'SELECT * FROM '.static::$table.'';
+  $query .= ($where_clause == '') ? '' : 'WHERE '.$where_clause;
+
+  $records_data = static::$conn->query($query);
+  while($record_data = $records_data->fetch_array())
+  {
+   $records[] = new $class_name($record_data[static::$index_column], $record_data);
+  }
+
+  return $records;
+ }
+
+ public function __construct($index_value, $values=[])
+ {
+  $this->index_value = $index_value;
+  $this->values = $values;
+
+  foreach($values as $column => $value)
+  {
+   if(!is_numeric($column))
+   {
+    $this->$column = $value;
+   }
+  }
+
+  if(static::$conn == null) static::init_conn();
+ }
+
+ public function getId()
+ {
+  return $this->index_value;
+ }
+
+ public function get($column)
+ {
+  if($column == static::$index_column)
+  {
+   return $this->index_value;
+  }
+  else if(isset($this->values[$column]))
+  {
+   return $this->values[$column];
+  }
+  else
+  {
+   $query = 'SELECT '.$column.' FROM '.static::$table.' WHERE '.static::$index_column.'='.$this->index_value;
+   $data = static::$conn->query($query);
+   $result = $data->fetch_array()[$column];
+   $this->values[$column] = $result;
+   return $result;
+  }
+ }
+
+ public function set($column, $new_value)
+ {
+  $this->values[$column] = $new_value;
+  $this->changed_values[$column] = $new_value;
+ }
+
+ public function save()
+ {
+  if($this->changed_values != null)
+  {
+   $query = 'UPDATE '.static::$table.' SET ';
+   foreach ($this->changed_values as $column => $value) 
+   {
+    $query .= ''.$column.'="'.$value.'" ';
+   }
+   $query .= ' WHERE '.static::$index_column.'='.$this->index_value;
+   static::$conn->query($query);
+  }
+ }
+}
